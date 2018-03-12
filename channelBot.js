a8ae2261d8d183b16e83b87977fa102987ee1e68
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-12 11:36:58 -0400

    V1 finished, needs testing

--- channelBot.js
+++ channelBot.js
@@ -6,11 +6,38 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("I'M");
 });
-client.channels.get("422238891115020289");
+client.channels.get("422238891115020289");//change the numbers with the
+//channel you want by using \#channel-name in Discord
 client.on('message', message => {
     if (channel.members.size >= 2) {
-      
+      function closeDownChannel(message) {
+    let channel = message.channel;
+    let roles = message.guild.roles; // collection
 
+    // find specific role - enter name of a role you create here
+    let testRole = roles.find('name', 'Gingkatest');
+
+    // overwrites 'SEND_MESSAGES' role, only on this specific channel
+    channel.overwritePermissions(
+        testRole,
+        { 'SEND_MESSAGES': true
+          'READ_MESSAGES': true },
+        // optional 'reason' for permission overwrite
+        'revealing!'
+    )
+    // handle responses / errors
+    .then(console.log)
+    .catch(console.log);
+       }//function closing
+    }//if closing
+ else if (channel.members.size <= 1){
+   channel.overwritePermissions(
+        testRole,
+        { 'SEND_MESSAGES': false
+          'READ_MESSAGES': false },
+        // optional 'reason' for permission overwrite
+        'hiding!'
+    )
 });
 
 client.login(token);
