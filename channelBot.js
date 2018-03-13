68e7dec18ca90f89b7a168017d1fec37e800072f
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-13 16:24:25 -0400

    Fixing part 1

--- channelBot.js
+++ channelBot.js
@@ -1,11 +1,11 @@
 
 const Discord = require("discord.js");
-const { token } = require('./config.json');
+const { token, channel } = require('./config.json');
 const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("I'M");
-  client.channels.get("422238891115020289");
+  client.channels.get("channel");
 });
 //change the numbers with the
 //channel you want by using \#channel-name in Discord
